d2f3739b0eb89dc2f52743ea7a8367effd694a0e
Aidan Swope <aidanswope@gmail.com>
2017-03-08 14:47:57 -0800

    Implemented opening book framework

--- src/opening.hpp
+++ src/opening.hpp
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "board.hpp"
+
+namespace asimov {
+
+    /**
+     * @brief An abstract class that represents a look-up table of openings.
+     *
+     * The constructor should load the book from a text file and the destructor
+     * should unload it.
+     */
+    class OpeningBook {
+    private:
+
+    public:
+        virtual ~OpeningBook();
+        virtual Move nextMove(Board* b); // Returns (-1, -1) if none is found
+        bool inBook = true; // Are we still getting moves from the book?
+    };
+}
